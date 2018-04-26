6d5ff39bb14e0da4ff9b51164eefac3521514fa1
huigod <tenghuiit@163.com>
2018-04-26 08:50:11 +0800

    Update code

--- src/main/java/com/huigod/netty/thirdexample/MyChatClientHandler.java
+++ src/main/java/com/huigod/netty/thirdexample/MyChatClientHandler.java
@@ -1,15 +1,13 @@
-package com.huigod.netty.secondexample;
+package com.huigod.netty.thirdexample;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import java.util.UUID;
 
-public class MyHttpServerHandler extends SimpleChannelInboundHandler<String> {
+public class MyChatClientHandler extends SimpleChannelInboundHandler<String> {
 
   @Override
   protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
-    System.out.println(ctx.channel().remoteAddress());
-    ctx.channel().writeAndFlush("from server:" + UUID.randomUUID());
+    System.out.println(msg);
   }
 
   @Override
